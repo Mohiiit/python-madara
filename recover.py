8475276a2cf4f253d7bca32a98fa8fe4f64960cc
mohiiit <mohit18343@iiitd.ac.in>
2024-04-24 00:42:45 +0530

    document the script

--- recover.py
+++ recover.py
@@ -12,22 +12,6 @@ import numpy as np
 from typing import List
 
 
-def to_hex_string(fft_output: List[int]) -> str:
-    return ''.join(format(num, 'x') for num in fft_output)
-
-
-def inverse_fft(res):
-    # Perform IFFT on the result 'res'
-    inverse_transform = np.fft.fft(res)
-    
-    # Convert the complex numbers to integers
-    inverse_integers = np.round(inverse_transform).astype(int)
-    
-    # Convert the integers to hexadecimal strings
-    blob_hex = ''.join(format(val, 'x').zfill(64) for val in inverse_integers)
-    
-    return blob_hex
-
 def ifft(arr, xs):
     if len(arr) == 1:
         return arr
@@ -47,21 +31,8 @@ def ifft(arr, xs):
 def div_mod(a, b, P):
     return (a * pow(b, P - 2, P)) % P
 
-
-# def fft(arr, xs):
-#     if len(arr) == 1:
-#         return arr
-#     n = len(arr) // 2
-#     even_terms = fft(arr[0::2], xs[0::2])
-#     odd_terms = fft(arr[1::2], xs[0::2])
-#     combined = [0] * len(arr)
-#     for i in range(n):
-#         even = even_terms[i]
-#         odd = odd_terms[i]
-#         combined[i] = (even + odd * pow(xs[i], 1, P)) % P
-#         combined[i + n] = (even - odd * pow(xs[i], 1, P)) % P
-#     return combined
-
+def to_hex_string(fft_output: List[int]) -> str:
+    return ''.join(format(num, 'x') for num in fft_output)
 
 def fft(arr, xs):
     if len(arr) == 1:
@@ -75,58 +46,53 @@ def fft(arr, xs):
         res[i + n] = div_mod(res0[i] - xs[i] * res1[i], 1, P)
     return res
 
-# def fft(arr, xs):
-#     if len(arr) == 1:
-#         return arr
-#     n = len(arr) // 2
-#     res0 = fft(arr[:n], xs[:n])
-#     res1 = fft(arr[n:], xs[:n])
-#     res = [0] * (2 * n)
-#     for i in range(n):
-#         res[i] = (res0[i] + xs[i] * res1[i]) % P
-#         res[i + n] = (res0[i] - xs[i] * res1[i]) % P
-#     return res
-
-# The div_mod function is not needed for the fft function
-
 # Blob data taken from this blob on Goerli:
-# https://goerli.blobscan.com/blob/0x01394306d3a5e6456771c2a4689e98269d220636723a877e44f19e11f6e57e6d
-# Read blob_hex from a file
+# https://etherscan.io/blob/0x017482460b39a8d4d52c22fee48adc76c2335cefdb2980fd6e3b7c65226ab171?bid=180082
+# Read blob data from a file
 with open('./blob4.txt', 'r') as file:
     blob_hex = file.read().strip()
 
+# Convert the hexadecimal blob data to integers
 data = [int(blob_hex[i:i+64], 16) for i in range(0, BLOB_LEN * 64, 64)]
-# print(data)
+
+# Write the data to a file, each integer on a new line
 with open("data_from_hex.txt", "w") as file:
-  for item in data:
-    file.write(str(item) + "\n")
+    for item in data:
+        file.write(str(item) + "\n")
+
+# Generate the evaluation points xs
 xs = [pow(z, int(bin(i)[2:].rjust(12, '0')[::-1], 2), P) for i in range(BLOB_LEN)]
+
+# Perform IFFT on the data
 res = ifft(data, xs)
 
+# Write the state differences to a file
 with open("state_diff5.txt", "w") as file:
-  for item in res:
-    file.write(str(item) + "\n")
+    for item in res:
+        file.write(str(item) + "\n")
 
-
+# Perform FFT on the IFFT result
 original_blob_hex = fft(res, xs)
 
+# Write the FFT result to a file
 with open("data_just_after_fft.txt", "w") as file:
-  for item in original_blob_hex:
-    file.write(str(item) + "\n")
+    for item in original_blob_hex:
+        file.write(str(item) + "\n")
+
+# Convert the FFT result to a hexadecimal string
 res = to_hex_string(original_blob_hex)
 
-
-# reverse_data = [int(res[i:i+64], 16) for i in range(0, BLOB_LEN * 64, 64)]
+# Perform IFFT on the FFT result to verify the original data is recovered
 reverse_res_data = ifft(original_blob_hex, xs)
 
+# Write the verification result to a file
 with open("state_diff_after_reverse.txt", "w") as file:
-  for item in reverse_res_data:
-    file.write(str(item) + "\n")
-    
+    for item in reverse_res_data:
+        file.write(str(item) + "\n")
+
+# Write the final hexadecimal string to a file
 with open("should_original_v2.txt", "w") as file:
     file.write(str(res) + "\n")
-# print("Original blob_hex:\n", original_blob_hex)
-# print(res, len(res))
 
 
 
